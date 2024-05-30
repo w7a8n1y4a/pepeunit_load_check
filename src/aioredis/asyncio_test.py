b2fe1a9055c20dac7e4c12629616bd0cb15d907c
Ivan Serebrennikov <admin@silberworks.com>
2024-05-30 03:10:17 +0300

    feat(tests): change conect method

--- src/aioredis/asyncio_test.py
+++ src/aioredis/asyncio_test.py
@@ -1,7 +1,12 @@
 import asyncio
+import random
 import time
 
 from redis import asyncio as aioredis
+
+
+keys = [str(item) for item in range(100)]
+
 
 async def async_range(count):
     for i in range(count):
@@ -9,23 +14,27 @@ async def async_range(count):
         await asyncio.sleep(0.0)
 
 
-async def redis_get_set(item):
-    redis = await aioredis.from_url('redis://127.0.0.1:6379', encoding="utf-8", decode_responses=True)
-    await redis.set('123', item)                                                        
-    redis_topic_value = await redis.get('123')
+
+async def redis_get_set(item, redis):
+    key = random.choice(keys)
+
+    await redis.set(key, item)
+    redis_topic_value = await redis.get(key)
 
     await asyncio.sleep(1)
-    
-    await redis.aclose()
 
     return redis_topic_value
 
 async def test():
     start = time.perf_counter()
-    for _ in range(10): 
-        results = await asyncio.gather(*(redis_get_set(item) for item in range(5000)))
-        print(len(results)) 
-    print(time.perf_counter() - start)
+
+    redis = await aioredis.from_url('redis://127.0.0.1:6379', encoding="utf-8", decode_responses=True)
+    for _ in range(10):
+        results = await asyncio.gather(*(redis_get_set(item, redis) for item in range(9998)))
+        print(len(results))
+
+    await redis.aclose()
+    print(f'avg_time: {(time.perf_counter() - start)/10}')
 
 asyncio.run(test())
 
